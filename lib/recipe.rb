498734c922e401e7692ebd3fa66f91c135c74abe
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-23 09:33:43 -0600

    Add add ingredient method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,12 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients
   def initialize(name)
     @name = name
+    @ingredients = []
   end
-  
+
+  def add_ingredient(ingredient, amount)
+    @ingredients << ingredient if !@ingredients.include?(ingredient)
+  end
+
 end
