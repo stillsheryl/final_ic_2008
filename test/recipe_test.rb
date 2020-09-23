93e293c8496b08873dc3178cd1a539276be2e45f
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-23 09:34:15 -0600

    Add ingredients required test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -22,12 +22,18 @@ class RecipeTest < Minitest::Test
     recipe1.add_ingredient(ingredient1, 4)
     recipe1.add_ingredient(ingredient2, 8)
 
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
+  end
+
+  def test_ingredients_required
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+
     expected = {
        ingredient1 => 6,
        ingredient2 => 8}
-    recipe1.ingredients_required
-
-    assert_equal [ingredient1, ingredient2], recipe1.ingredients
+    assert_equal expected, recipe1.ingredients_required
   end
 
 end
