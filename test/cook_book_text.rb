68801c18b53d0c314110c932c63c3f676c3fb15b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-23 10:39:59 -0600

    Add test ingredients by calories

--- test/cook_book_text.rb
+++ test/cook_book_text.rb
@@ -50,7 +50,7 @@ class CookBookTest < Minitest::Test
 
   end
 
-  def test_summary
+  def test_list_ingredients_by_calories
     cookbook = CookBook.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
@@ -66,8 +66,25 @@ class CookBookTest < Minitest::Test
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
 
-    # assert_equal [{:ingredient=>"Macaroni", :amount=>"8 oz"},
-    #   {:ingredient=>"Cheese", :amount=>"2 C"}], cookbook.ingredients_by_calories
+    assert_equal [{:ingredient=>"Macaroni", :amount=>"8 oz"},
+      {:ingredient=>"Cheese", :amount=>"2 C"}], cookbook.ingredients_by_calories
+  end
+
+  def test_summary
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+    recipe2 = Recipe.new("Burger")
+
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 100)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
 
     expected = [{:name=>"Mac and Cheese", :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}}, {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
     assert_equal expected, cookbook.summary
